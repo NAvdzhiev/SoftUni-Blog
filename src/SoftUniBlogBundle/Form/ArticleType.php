d5b6e3fa836397dde47473c13cacebc522878267
Nedyalko <n_avdzhiev@students.softuni.bg>
2016-12-20 13:23:06 +0200

    Add files via upload

--- src/SoftUniBlogBundle/Form/ArticleType.php
+++ src/SoftUniBlogBundle/Form/ArticleType.php
@@ -2,7 +2,10 @@
 
 namespace SoftUniBlogBundle\Form;
 
+use SoftUniBlogBundle\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,7 +16,10 @@ class ArticleType extends AbstractType
     {
         $builder
             ->add('title', TextType::class)
-            ->add('content', TextType::class);
+            ->add('content', TextType::class)
+            ->add('category', EntityType::class, ['class' => Category::class])
+            ->add('coverPhoto', FileType::class, array('label' => 'Cover Photo (JPG file)',
+                'data_class' => null));
     }
 
     public function configureOptions(OptionsResolver $resolver)
